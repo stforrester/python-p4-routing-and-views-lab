bb8aee77ad744cc6bafda8a9b8a38672542f601d
Stuart Forrester <stforrester@gmail.com>
2025-01-21 15:27:19 -0600

    completes lab and passes all tests

--- server/app.py
+++ server/app.py
@@ -6,3 +6,33 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    response = ''
+    for int in range(parameter):
+        response += f'{int}\n'
+    return response
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return (f'{num1 + num2}')
+    elif operation == '-':
+        return (f'{num1 - num2}')
+    elif operation == '*':
+        return (f'{num1 * num2}')
+    elif operation == 'div':
+        return (f'{num1 / num2}')
+    elif operation == '%':
+        return (f'{num1 % num2}')
+    
